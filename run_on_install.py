1b717e7e81c8cdf64b0f61fb08dbf726718280a2
Jake Gaylor <jhgaylor@gmail.com>
2014-11-27 00:23:30 -0600

    takes user input

--- run_on_install.py
+++ run_on_install.py
@@ -1,14 +1,20 @@
 import os
+from slugify import slugify
+
+
 USER_PATH = os.path.expanduser('~')
 
+school_name = raw_input("What is the schools name?") or "University of Alabama at Birmingham"
+school_name = slugify(school_name)
 
 GOOGLE_CHROME_PACKAGE_NAME = "google-chrome-stable_current_amd64.deb"
 GOOGLE_CHROME_PACKAGE_PATH = os.path.join(USER_PATH, GOOGLE_CHROME_PACKAGE_NAME)
-SCHOOL_STREAMER_URL = "http://socialdrizzle.com/univeristy-of-alabama-at-birmingham/s/jumbotron"
+SCHOOL_STREAMER_URL = "http://socialdrizzle.com/"+school_name+"/s/jumbotron"
 STARTUP_DESKTOP_SCRIPT_PATH = os.path.join(USER_PATH, "Desktop/Start-Drizzle.sh")
 AUTORUN_SCRIPT_PATH = os.path.join(USER_PATH, ".config/autostart")
 AUTORUN_SCRIPT_NAME = os.path.join(AUTORUN_SCRIPT_PATH, "drizzle.desktop")
 
+print SCHOOL_STREAMER_URL
 # installs/removes
 def install_software():
     try:
@@ -46,8 +52,9 @@ def make_startup_files():
     except OSError as oserr:
         if oserr:
             raise 
+
     
 
-install_software()
+# install_software()
 
-make_startup_files()
+# make_startup_files()
