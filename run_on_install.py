4335c0556188b69db79e712eb8b4232fcbe806ce
steven <steven.barber@eagles.usm.edu>
2014-11-26 18:42:49 -0600

    run this .py to setup NeD

--- run_on_install.py
+++ run_on_install.py
@@ -1,31 +1,45 @@
 import os
 
 GOOGLE_CHROME_PACKAGE_NAME = "google-chrome-stable_current_amd64.deb"
-GOOGLE_CHROME_PACKAGE_PATH = "~/$GOOGLE_CHROME_PACKAGE_NAME"
+GOOGLE_CHROME_PACKAGE_PATH = os.path.expanduser("~")+"/"+GOOGLE_CHROME_PACKAGE_NAME
 SCHOOL_STREAMER_URL = "http://socialdrizzle.com/univeristy-of-alabama-at-birmingham/s/jumbotron"
-STARTUP_SCRIPT_PATH = "~/Desktop/Start-Drizzle.sh"
+STARTUP_DESKTOP_SCRIPT_PATH = os.path.expanduser("~")+"/Desktop/Start-Drizzle.sh"
+AUTORUN_SCRIPT_PATH = os.path.expanduser("~")+"/.config/autostart/directory"
+AUTORUN_SCRIPT_NAME = AUTORUN_SCRIPT_PATH+"/drizzle.desktop"
 
 #installs/removes
-# install chromium
-os.system("sudo apt-get install chromium-browser")
-# Install Chrome dependencies
-os.system("sudo apt-get install libxss1 libappindicator1 libindicator7")
-# download chrome's debian package
-os.system("wget -O $GOOGLE_CHROME_PACKAGE_PATH "https://dl.google.com/linux/direct/$GOOGLE_CHROME_PACKAGE_NAME"")
-# install the chrome package
-os.system("sudo dpkg -i $GOOGLE_CHROME_PACKAGE_NAME")
-# remove the google deb package that was downloaded and installed already
-os.system("sudo rm -rf $GOOGLE_CHROME_PACKAGE_PATH")
+def install_software():
+    try:
+        # install chromium
+        os.system("sudo apt-get install chromium-browser")
+        # Install Chrome dependencies
+        os.system("sudo apt-get install libxss1 libappindicator1 libindicator7")
+        # download chrome's debian package
+        os.system("wget -O "+GOOGLE_CHROME_PACKAGE_PATH+" https://dl.google.com/linux/direct/$GOOGLE_CHROME_PACKAGE_NAME")
+        # install the chrome package
+        os.system("sudo dpkg -i "+GOOGLE_CHROME_PACKAGE_NAME)
+        # remove the google deb package that was downloaded and installed already
+        os.system("sudo rm -rf "+GOOGLE_CHROME_PACKAGE_PATH)
+    except OSError as oserr:
+        if oserr:
+            raise 
 
-# writes Start-Drizzle.sh to Desktop to open on boot
-os.system("echo "#!/bin/bash \n
-chromium-browser --kiosk ‘$SCHOOL_STREAMER_URL’" > $STARTUP_SCRIPT_PATH")
 
-# change permissions on the startup script
-os.system("chmod 755 $STARTUP_SCRIPT_PATH")
+def make_startup():
+    try:
+        startup_desktop = open(STARTUP_DESKTOP_SCRIPT_PATH, "w")
+        startup_desktop.write("#!/bin/bash\nchromium-browser --kiosk " + SCHOOL_STREAMER_URL)
+        os.system("chmod 755 "+STARTUP_DESKTOP_SCRIPT_PATH)
 
-# autorun the startup script once the desktop is ready
-# os.system("mkdir -p ~/.config/autostart/directory/
-# echo "[Desktop Entry]
-# Type=Application
-# Exec=$STARTUP_SCRIPT_PATH" > ~/.config/autostart/directory/drizzle.desktop")
+        os.makedirs(AUTORUN_SCRIPT_PATH)
+        startup_config = open(AUTORUN_SCRIPT_NAME, "w")
+        startup_config.write("[Desktop Entry]\n\nType=Application\n\nExec="+STARTUP_DESKTOP_SCRIPT_PATH)
+        
+    except OSError as oserr:
+        if oserr:
+            raise 
+    
+
+#install_software()
+
+make_startup()
