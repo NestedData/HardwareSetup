e0ad6d80424cc134e12462d895b06893d234858a
steven <steven.barber@eagles.usm.edu>
2014-12-12 15:08:10 -0600

    fix xorg.conf.d

--- killDPMS.py
+++ killDPMS.py
@@ -5,5 +5,5 @@ DPMS_PATH = "/etc/X11/"
 
 if not os.path.exists(DPMS_PATH):   
 	os.makedirs(DPMS_PATH)
-	DPMS_PATH = DPMS_PATH + "xorg.conf.d"
-shutil.copyfile(DPMS_PATH, "xorg.conf.d")
+	
+shutil.copyfile("xorg.conf.d", DPMS_PATH)
