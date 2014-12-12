ec917c6a796408ac7318799d7f54690ad2bfa7fb
steven <steven.barber@eagles.usm.edu>
2014-12-12 14:32:17 -0600

    if directory isn't there, create it

--- killDPMS.py
+++ killDPMS.py
@@ -1,6 +1,9 @@
 import os
 import shutil
 
-DPMS_PATH = "/etc/X11/xorg.conf.d"
+DPMS_PATH = "/etc/X11/"
 
+if not os.path.exists(DPMS_PATH):   
+	os.makedirs(DPMS_PATH)
+	DPMS_PATH = DPMS_PATH + "xorg.conf.d"
 shutil.copyfile(DPMS_PATH, "xorg.conf.d")
