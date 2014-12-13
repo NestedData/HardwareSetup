08afbe66d5c7df15c3e67c2183dfbdff613104ac
steven <steven.barber@eagles.usm.edu>
2014-12-12 19:49:10 -0600

    final adjustments

--- new/client.conf.d
+++
@@ -1,7 +0,0 @@
-Section "ServerFlags"
-Option "BlankTime" "0"
-Option "StandbyTime" "0"
-Option "SuspendTime" "0"
-Option "OffTime" "0"
-EndSection[int32] ShowTaskbarInfoOnMinimize = 0[int32] ShowTaskbarInfoOnMinimize = 0
-[int32] ShowTaskbarInfoOnMinimize = 0
